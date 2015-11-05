04982ca5d7efe9279a2c1cacd1166e0db8d833ff
Pete1232 <pete1232@gmail.com>
2015-11-05 12:14:44 +0000

    Updating test classes and pom

--- src/test/scala/com/qa/wotsunit/DummyBuilderSpec.scala
+++ src/test/scala/com/qa/wotsunit/DummyBuilderSpec.scala
@@ -1,9 +1,14 @@
 package com.qa.wotsunit
 
+import com.qa.dummydata.DummyBuilder
+import scala.util.Random
+
 /**
  * @author pnewman
  */
 class DummyBuilderSpec extends UnitSpec{
-  "Calling addToArray" should "populate the given index of databaseArray with an entity of the given type" //TODO Implement test
+  val entries = Math.abs(Random.nextInt(200))
+  "Calling addToArray" should "populate the given index of databaseArray with an entity of the given type" in {
+  }
   "Calling buildEntityArray" should "populate ALL entries of databaseArray with entities of the given type" //TODO Implement test
 }
--- src/test/scala/com/qa/wotsunit/GenericRepositoryDummySpec.scala
+++ src/test/scala/com/qa/wotsunit/GenericRepositoryDummySpec.scala
@@ -3,21 +3,39 @@ package com.qa.wotsunit
 import org.scalatest._
 import com.qa.repositoryimplementations.GenericRepositoryDummy
 import com.qa.entities.Product
+import com.qa.entities.Employee
+import com.qa.entities.CustomerOrder
+import com.qa.entities.PurchaseOrder
+import com.qa.entities.CustomerOrder
+import com.qa.entities.Employee
+import com.qa.entities.PurchaseOrder
 
 class GenericRepositoryDummySpec extends UnitSpec{
+    val customerOrder = new CustomerOrder
+    val employee = new Employee
+    val product = new Product
+    val purchaseOrder = new PurchaseOrder
+    val grd = new GenericRepositoryDummy
   //TODO Add more methods as they are made
   "Calling getEntityList" should "return an array of the given entity type" in{
-    //TODO Add to this with more entities as they are made
-    val grd = new GenericRepositoryDummy
-    val product = new Product
-    val array:Array[Product] = grd.getEntityList(product)
-    array.getClass.getSimpleName+"" should be ("Product[]")
+    val arrayCO:Array[CustomerOrder] = grd.getEntityList(customerOrder)
+    val arrayE:Array[Employee] = grd.getEntityList(employee)
+    val arrayP:Array[Product] = grd.getEntityList(product)
+    val arrayPO:Array[PurchaseOrder] = grd.getEntityList(purchaseOrder)
+    arrayCO.getClass.getSimpleName+"" should be ("CustomerOrder[]")
+    arrayE.getClass.getSimpleName+"" should be ("Employee[]")
+    arrayP.getClass.getSimpleName+"" should be ("Product[]")
+    arrayPO.getClass.getSimpleName+"" should be ("PurchaseOrder[]")
   }
   "Calling findAll" should "return an array of the given entity type" in{
-    //TODO Add to this with more entities as they are made
     val grd = new GenericRepositoryDummy
-    val product = new Product
-    val array:Array[Product] = grd.findAll(product)
-    array.getClass.getSimpleName+"" should be ("Product[]")
+    val arrayCO:Array[CustomerOrder] = grd.findAll(customerOrder)
+    val arrayE:Array[Employee] = grd.findAll(employee)
+    val arrayP:Array[Product] = grd.findAll(product)
+    val arrayPO:Array[PurchaseOrder] = grd.findAll(purchaseOrder)
+    arrayCO.getClass.getSimpleName+"" should be ("CustomerOrder[]")
+    arrayE.getClass.getSimpleName+"" should be ("Employee[]")
+    arrayP.getClass.getSimpleName+"" should be ("Product[]")
+    arrayPO.getClass.getSimpleName+"" should be ("PurchaseOrder[]")
   }
 }
