bb2a33b0e1aada071a633318bb8bc1e4414a7a79
Pete1232 <pete1232@gmail.com>
2015-11-15 14:17:11 +0000

    Removed some excess code

--- src/main/scala/com/qa/repositories/GenericRepositoryActual.scala
+++ src/main/scala/com/qa/repositories/GenericRepositoryActual.scala
@@ -24,175 +24,153 @@ import java.sql.SQLException
 /**
  * @author pnewman
  */
-/*class GenericRepositoryActual[E: Manifest] {
+object GenericRepositoryActual extends GenericRepository{
   val logger = Logger(LoggerFactory.getLogger("GenericRepositoryMongo.class"))
-  val mongoClient = MongoClient("localhost", 27017)
-  val mongoDB = mongoClient("FreshTech")
-  val driverSQL = "com.mysql.jdbc.Driver"
-  val urlSQL = "jdbc:mysql://localhost/NBGardens"
-  val usernameSQL = "root"
-  val passwordSQL = "academy"*/
-
-  object GenericRepositoryActual extends GenericRepository{
-    val logger = Logger(LoggerFactory.getLogger("GenericRepositoryMongo.class"))
-    val mongoDB = DataConfig.connectionMongo
-    
-    def getDatabaseCustomerOrder: Array[CustomerOrder] = {
-      val conn = DataConfig.connectionSQL
-      val stmt = conn.createStatement
-      val ps: PreparedStatement = conn.prepareStatement("SELECT * FROM customerorder")
-      logger.debug("Statement: " + ps)
-      val rs = ps.executeQuery
-      def createOrders(orders: Array[CustomerOrder]): Array[CustomerOrder] = {
-        if (rs.next) {
-          val orderId = rs.getInt("CustomerOrderId")
-          val orderStatus = rs.getString("CustomerOrderStatus")
-          val deliveryAddress = rs.getString("AddressId")
-          val employeeId = rs.getInt("EmployeeId")
-          val newOrder = new CustomerOrder(orderId, orderStatus, deliveryAddress, employeeId)
-          createOrders(orders :+ newOrder)
-        } else {
-          orders
-        }
-      }
-      createOrders(Array[CustomerOrder]())
-    }
-    
-    def getDatabasePurchaseOrder: Array[PurchaseOrder] = {
-      val conn = DataConfig.connectionSQL
-      val stmt = conn.createStatement
-      val ps: PreparedStatement = conn.prepareStatement("SELECT * FROM purchaseorder")
-      logger.debug("Statement: " + ps)
-      val rs = ps.executeQuery
-      def createOrders(orders: Array[PurchaseOrder]): Array[PurchaseOrder] = {
-        if (rs.next) {
-          val orderId = rs.getInt("PurchaseOrderId")
-          val employeeId = rs.getInt("Employee_EmployeeId")
-          val orderStatus = rs.getString("OrderStatus")
-          val newOrder = new PurchaseOrder(orderId,employeeId,orderStatus)
-          createOrders(orders :+ newOrder)
-        } else {
-          orders
-        }
-      }
-      createOrders(Array[PurchaseOrder]())
-    }
-    
-    def getDatabaseEmployee: Array[Employee] = {
-      val conn = DataConfig.connectionSQL
-      val stmt = conn.createStatement
-      val ps: PreparedStatement = conn.prepareStatement("SELECT * FROM employee")
-      logger.debug("Statement: " + ps)
-      val rs = ps.executeQuery
-      //logger.debug("Result set: "+rs.next)
-      def createEmployees(employees: Array[Employee]): Array[Employee] = {
-        if (rs.next) {
-          //logger.debug("Result set next exists")
-          val employeeId = rs.getInt("EmployeeId")
-          //logger.debug("Order Id: "+orderId)
-          val employeeName = rs.getString("EmployeeName")
-          val employeeUsername = rs.getString("EmployeeUsername")
-          val employeePassword = rs.getString("EmployeePassword")
-          val employeeAccessLevel = rs.getInt("AccessLevel")
-          //logger.debug("Employee Id: "+employeeId)
-          val newEmployee = new Employee(employeeId, employeeName, employeeUsername, employeePassword, employeeAccessLevel)
-          //logger.debug("New customer order: "+newOrder)
-          createEmployees(employees :+ newEmployee)
-        } else {
-          employees
-        }
-      }
-      createEmployees(Array[Employee]())
-    }
-    def getDatabaseCustomerOrderLine(customerOrderId: Int): Array[CustomerOrderLine] = {
-      val conn = DataConfig.connectionSQL
-      val stmt = conn.createStatement
-      val ps: PreparedStatement = conn.prepareStatement("SELECT * FROM customerorderline WHERE CustomerOrder_CustomerOrderId="+customerOrderId)
-      logger.debug("Statement: " + ps)
-      val rs = ps.executeQuery
-      //logger.debug("Result set: "+rs.next)
-      def createCustomerOrderLine(orderLines: Array[CustomerOrderLine]): Array[CustomerOrderLine] = {
-        if (rs.next) {
-          //logger.debug("Result set next exists")
-          val orderId = rs.getInt("CustomerOrder_CustomerOrderId")
-          //logger.debug("Order Id: "+orderId)
-          val productId = rs.getInt("ProductID")
-          val quantity = rs.getInt("Quantity")
-          //logger.debug("Employee Id: "+employeeId)
-          val newOrderLine = new CustomerOrderLine(orderId,productId,quantity)
-          //logger.debug("New customer order: "+newOrder)
-          createCustomerOrderLine(orderLines :+ newOrderLine)
-        } else {
-          orderLines
-        }
-      }
-      createCustomerOrderLine(Array[CustomerOrderLine]())
-    }
-    
-    def getProductByOrderLine(orderLine:CustomerOrderLine):Product={
-      val productDoc = DataConfig.connectionMongo("Product").find.toArray
-      def findProduct(count:Int):Product = {
-        val productId = (productDoc(count).get("productID") + "").toInt
-        if(productId==orderLine.productId_){
-          val productName = productDoc(count).get("productName") + ""
-          val image = productDoc(count).get("image") + ""
-          val porousware = (productDoc(count).get("porusware") + "").toBoolean
-          val orderId = orderLine.orderId_
-          val aisle: Char = (productDoc(count).get("aisle") + "")(0)
-          val shelf = (productDoc(count).get("shelf") + "")toInt
-          val quantity = orderLine.quantity_
-          new Product(productId, productName, image, porousware, orderId, aisle, shelf, quantity)
-        }
-        else{
-          findProduct(count.+(1))
-        }
-      }
-      findProduct(0)
-    }
-    
-    def persist[E:Manifest]: E => Unit = { entity =>
-      logger.debug("Persisting entity of type {}", entity.getClass.getSimpleName)
-      def postAppend(array: Array[E]): Array[E] = {
-        array :+ entity
-      }
-      entity match {
-        case entity: CustomerOrder =>
-        case entity: Employee      =>
-        case entity: Product       =>
-        case entity: PurchaseOrder =>
-        case _ => {
-          logger.error("Entity of type {} not handled by persist method", entity.getClass.getSimpleName)
-          logger.warn("Method will return null")
-          null.asInstanceOf[Array[E]]
-        }
+  val mongoDB = DataConfig.connectionMongo
+  
+  def getDatabaseCustomerOrder: Array[CustomerOrder] = {
+    val conn = DataConfig.connectionSQL
+    val stmt = conn.createStatement
+    val ps: PreparedStatement = conn.prepareStatement("SELECT * FROM customerorder")
+    logger.debug("Statement: " + ps)
+    val rs = ps.executeQuery
+    def createOrders(orders: Array[CustomerOrder]): Array[CustomerOrder] = {
+      if (rs.next) {
+        val orderId = rs.getInt("CustomerOrderId")
+        val orderStatus = rs.getString("CustomerOrderStatus")
+        val deliveryAddress = rs.getString("AddressId")
+        val employeeId = rs.getInt("EmployeeId")
+        val newOrder = new CustomerOrder(orderId, orderStatus, deliveryAddress, employeeId)
+        createOrders(orders :+ newOrder)
+      } else {
+        orders
       }
     }
-    
-    def persistArray[E:Manifest]: Array[E] => Unit = { entityArray =>
-      def postAppendArray(count: Int, array: Array[E]): Array[E] = {
-        if (count < entityArray.length) {
-          logger.debug("persistArray: start array length: {}", array.length + "")
-          val nextArray = array :+ entityArray(count)
-          logger.debug("persistArray: end array length: {}", nextArray.length + "")
-          postAppendArray(count + 1, nextArray)
-        } else {
-          array
-        }
+    createOrders(Array[CustomerOrder]())
+  }
+  
+  def getDatabasePurchaseOrder: Array[PurchaseOrder] = {
+    val conn = DataConfig.connectionSQL
+    val stmt = conn.createStatement
+    val ps: PreparedStatement = conn.prepareStatement("SELECT * FROM purchaseorder")
+    logger.debug("Statement: " + ps)
+    val rs = ps.executeQuery
+    def createOrders(orders: Array[PurchaseOrder]): Array[PurchaseOrder] = {
+      if (rs.next) {
+        val orderId = rs.getInt("PurchaseOrderId")
+        val employeeId = rs.getInt("Employee_EmployeeId")
+        val orderStatus = rs.getString("OrderStatus")
+        val newOrder = new PurchaseOrder(orderId,employeeId,orderStatus)
+        createOrders(orders :+ newOrder)
+      } else {
+        orders
       }
-      val testEntity = entityArray.head
-      update
     }
-    
-    def update[E:Manifest]: (E, Int) => Unit = { (entity, index) =>
-      entity match {
-        case entity: CustomerOrder =>
-        case entity: Employee      =>
-        case entity: Product       =>
-        case entity: PurchaseOrder =>
-        case _ => {
-          logger.error("Entity of type {} not handled by update method", entity.getClass.getSimpleName)
-          logger.warn("Method will return null")
-        }
+    createOrders(Array[PurchaseOrder]())
+  }
+  
+  def getDatabaseEmployee: Array[Employee] = {
+    val conn = DataConfig.connectionSQL
+    val stmt = conn.createStatement
+    val ps: PreparedStatement = conn.prepareStatement("SELECT * FROM employee")
+    logger.debug("Statement: " + ps)
+    val rs = ps.executeQuery
+    def createEmployees(employees: Array[Employee]): Array[Employee] = {
+      if (rs.next) {
+        val employeeId = rs.getInt("EmployeeId")
+        val employeeName = rs.getString("EmployeeName")
+        val employeeUsername = rs.getString("EmployeeUsername")
+        val employeePassword = rs.getString("EmployeePassword")
+        val employeeAccessLevel = rs.getInt("AccessLevel")
+        val newEmployee = new Employee(employeeId, employeeName, employeeUsername, employeePassword, employeeAccessLevel)
+        createEmployees(employees :+ newEmployee)
+      } else {
+        employees
+      }
+    }
+    createEmployees(Array[Employee]())
+  }
+  def getDatabaseCustomerOrderLine(customerOrderId: Int): Array[CustomerOrderLine] = {
+    val conn = DataConfig.connectionSQL
+    val stmt = conn.createStatement
+    val ps: PreparedStatement = conn.prepareStatement("SELECT * FROM customerorderline WHERE CustomerOrder_CustomerOrderId="+customerOrderId)
+    val rs = ps.executeQuery
+    def createCustomerOrderLine(orderLines: Array[CustomerOrderLine]): Array[CustomerOrderLine] = {
+      if (rs.next) {
+        val orderId = rs.getInt("CustomerOrder_CustomerOrderId")
+        val productId = rs.getInt("ProductID")
+        val quantity = rs.getInt("Quantity")
+        val newOrderLine = new CustomerOrderLine(orderId,productId,quantity)
+        createCustomerOrderLine(orderLines :+ newOrderLine)
+      } else {
+        orderLines
+      }
+    }
+    createCustomerOrderLine(Array[CustomerOrderLine]())
+  }
+  
+  def getProductByOrderLine(orderLine:CustomerOrderLine):Product={
+    val productDoc = DataConfig.connectionMongo("Product").find.toArray
+    def findProduct(count:Int):Product = {
+      val productId = (productDoc(count).get("productID") + "").toInt
+      if(productId==orderLine.productId_){
+        val productName = productDoc(count).get("productName") + ""
+        val image = productDoc(count).get("image") + ""
+        val porousware = (productDoc(count).get("porusware") + "").toBoolean
+        val orderId = orderLine.orderId_
+        val aisle: Char = (productDoc(count).get("aisle") + "")(0)
+        val shelf = (productDoc(count).get("shelf") + "")toInt
+        val quantity = orderLine.quantity_
+        new Product(productId, productName, image, porousware, orderId, aisle, shelf, quantity)
+      }
+      else{
+        findProduct(count.+(1))
+      }
+    }
+    findProduct(0)
+  }
+  
+  def persist[E:Manifest]: E => Unit = { entity =>
+    logger.debug("Persisting entity of type {}", entity.getClass.getSimpleName)
+    def postAppend(array: Array[E]): Array[E] = {
+      array :+ entity
+    }
+    entity match {
+      case entity: CustomerOrder =>
+      case entity: Employee      =>
+      case entity: Product       =>
+      case entity: PurchaseOrder =>
+      case _ => {
+        logger.error("Entity of type {} not handled by persist method", entity.getClass.getSimpleName)
+        logger.warn("Method will return null")
+        null.asInstanceOf[Array[E]]
       }
     }
   }
+  
+  def persistArray[E:Manifest]: Array[E] => Unit = { entityArray =>
+    def postAppendArray(count: Int, array: Array[E]): Array[E] = {
+      if (count < entityArray.length) {
+        val nextArray = array :+ entityArray(count)
+        postAppendArray(count + 1, nextArray)
+      } else {
+        array
+      }
+    }
+    val testEntity = entityArray.head
+    update
+  }
+  
+  def update[E:Manifest]: (E, Int) => Unit = { (entity, index) =>
+    entity match {
+      case entity: CustomerOrder =>
+      case entity: Employee      =>
+      case entity: Product       =>
+      case entity: PurchaseOrder =>
+      case _ => {
+        logger.error("Entity of type {} not handled by update method", entity.getClass.getSimpleName)
+        logger.warn("Method will return null")
+      }
+    }
+  }
+}
--- src/test/scala/com/qa/wotsunit/DummyBuilderSpec.scala
+++ src/test/scala/com/qa/wotsunit/DummyBuilderSpec.scala
@@ -12,58 +12,65 @@ import com.qa.entities.PurchaseOrder
  */
 class DummyBuilderSpec extends UnitSpec{
 
-  "Calling addToArray" should "populate the given index of databaseArray (of any size) with an entity of the given type" in {
-    val entries = Math.abs(Random.nextInt(200))
-    val index = Math.abs(Random.nextInt(entries))
-    val builderCO = new DummyBuilder[CustomerOrder](entries)
-    val builderE = new DummyBuilder[Employee](entries)
-    val builderP = new DummyBuilder[Product](entries)
-    val builderPO = new DummyBuilder[PurchaseOrder](entries)
-    val customerOrder=new CustomerOrder
-    val employee=new Employee
-    val product=new Product
-    val purchaseOrder=new PurchaseOrder
-  
-    builderCO.addToArray(index,customerOrder)
-    builderCO.databaseArray(index) should be (customerOrder)
-    builderE.addToArray(index,employee)
-    builderE.databaseArray(index) should be (employee)
-    builderP.addToArray(index,product)
-    builderP.databaseArray(index) should be (product)
-    builderPO.addToArray(index,purchaseOrder)
-    builderPO.databaseArray(index) should be (purchaseOrder)
-  }
-  "Calling buildEntityArray" should "populate ALL entries of databaseArray (of any size) with entities of the given type (when starting with counter 0)" in {
-    val entries = Math.abs(Random.nextInt(200))
-    val index = Math.abs(Random.nextInt(entries))
-    val builderCO = new DummyBuilder[CustomerOrder](entries)
-    val builderE = new DummyBuilder[Employee](entries)
-    val builderP = new DummyBuilder[Product](entries)
-    val builderPO = new DummyBuilder[PurchaseOrder](entries)
-    val customerOrder=new CustomerOrder
-    val employee=new Employee
-    val product=new Product
-    val purchaseOrder=new PurchaseOrder
-  
-    builderCO.buildEntityArray(0, customerOrder)  
-    for(entry<-builderCO.databaseArray){
-      entry should not be (null)
-      entry.getClass should be (customerOrder.getClass)
-    }
-    builderE.buildEntityArray(0, employee)
-    for(entry<-builderE.databaseArray){
-      entry should not be (null)
-      entry.getClass should be (employee.getClass)
-    }
-    builderP.buildEntityArray(0, product)
-    for(entry<-builderP.databaseArray){
-      entry should not be (null)
-      entry.getClass should be (product.getClass)
-    }
-    builderPO.buildEntityArray(0, purchaseOrder)
-    for(entry<-builderPO.databaseArray){
-      entry should not be (null)
-      entry.getClass should be (purchaseOrder.getClass)
+  def testAddToArray{
+    "Calling addToArray" should "populate the given index of databaseArray (of any size) with an entity of the given type" in {
+      val entries = Math.abs(Random.nextInt(200))
+      val index = Math.abs(Random.nextInt(entries))
+      val builderCO = new DummyBuilder[CustomerOrder](entries)
+      val builderE = new DummyBuilder[Employee](entries)
+      val builderP = new DummyBuilder[Product](entries)
+      val builderPO = new DummyBuilder[PurchaseOrder](entries)
+      val customerOrder=new CustomerOrder
+      val employee=new Employee
+      val product=new Product
+      val purchaseOrder=new PurchaseOrder
+    
+      builderCO.addToArray(index,customerOrder)
+      builderCO.databaseArray(index) should be (customerOrder)
+      builderE.addToArray(index,employee)
+      builderE.databaseArray(index) should be (employee)
+      builderP.addToArray(index,product)
+      builderP.databaseArray(index) should be (product)
+      builderPO.addToArray(index,purchaseOrder)
+      builderPO.databaseArray(index) should be (purchaseOrder)
     }
   }
+  
+  def testBuildEntityArray{
+    "Calling buildEntityArray" should "populate ALL entries of databaseArray (of any size) with entities of the given type (when starting with counter 0)" in {
+      val entries = Math.abs(Random.nextInt(200))
+      val index = Math.abs(Random.nextInt(entries))
+      val builderCO = new DummyBuilder[CustomerOrder](entries)
+      val builderE = new DummyBuilder[Employee](entries)
+      val builderP = new DummyBuilder[Product](entries)
+      val builderPO = new DummyBuilder[PurchaseOrder](entries)
+      val customerOrder=new CustomerOrder
+      val employee=new Employee
+      val product=new Product
+      val purchaseOrder=new PurchaseOrder
+    
+      builderCO.buildEntityArray(0, customerOrder)  
+      for(entry<-builderCO.databaseArray){
+        entry should not be (null)
+        entry.getClass should be (customerOrder.getClass)
+      }
+      builderE.buildEntityArray(0, employee)
+      for(entry<-builderE.databaseArray){
+        entry should not be (null)
+        entry.getClass should be (employee.getClass)
+      }
+      builderP.buildEntityArray(0, product)
+      for(entry<-builderP.databaseArray){
+        entry should not be (null)
+        entry.getClass should be (product.getClass)
+      }
+      builderPO.buildEntityArray(0, purchaseOrder)
+      for(entry<-builderPO.databaseArray){
+        entry should not be (null)
+        entry.getClass should be (purchaseOrder.getClass)
+      }
+    }
+  }
+  testAddToArray
+  testBuildEntityArray
 }
